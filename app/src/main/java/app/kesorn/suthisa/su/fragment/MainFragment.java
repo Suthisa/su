67b9d1fd60673d13f05bde1a2ae0be0eac0c3ddb
SuthisaK <su3cute5@gmail.com>
2017-10-10 11:59:45 +0700

    Before Lunch

---
+++ app/src/main/java/app/kesorn/suthisa/su/fragment/MainFragment.java
@@ -0,0 +1,30 @@
+package app.kesorn.suthisa.su.fragment;
+
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.kesorn.suthisa.su.R;
+
+
+/**
+ * Created by suthisa on 10/10/2017.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_main,container,false);
+
+
+        return view;
+    }
+}//Main Class
