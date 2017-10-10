fda6085823955929e39f1728fa88fbcdcab2aa28
SuthisaK <su3cute5@gmail.com>
2017-10-10 14:24:27 +0700

    add Fragment success

--- app/src/main/java/app/kesorn/suthisa/su/MainActivity.java
+++ app/src/main/java/app/kesorn/suthisa/su/MainActivity.java
@@ -3,12 +3,21 @@ package app.kesorn.suthisa.su;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.kesorn.suthisa.su.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Add Fragment to Activity
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+
+        }
+
     }//Main Method
 
 } //Main Class
